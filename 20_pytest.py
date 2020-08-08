da6fa1114a4af721158ef4e621126d2d2fa20255
Luis Rivas <luis.m.rivas@oracle.com>
2020-08-08 13:57:14 -0500

    add 20 example

---
+++ 20_pytest.py
@@ -0,0 +1,29 @@
+import pytest
+import requests
+import logging
+
+
+base_url = 'http://127.0.0.1:5000/books'
+
+
+def test_print():
+    print("something")
+
+
+
+@pytest.mark.parametrize("a", [ ("name"), ("test"), ("test3"), ("test4") ])
+def test_add(a):
+    new_book = {'name': a[0], 'author': 'Tester'}
+    response = requests.post(base_url, json=new_book)
+    assert response.status_code == 200, 'Create book should return 200'
+    json = response.json()
+    assert json['id'], 'A new id should be generated'
+    assert json['name'] == a[0], 'New book name should be TestBook'
+    assert json['author'] == 'Tester', 'New book author should be Tester'
+    assert json['views'] == 0, 'New book views should be 0'
+    logging.info(f'New Book: {json}')
+
+
+@pytest.mark.parametrize("a", [ (1), (2), (3), (4) ])
+def test_number(a):
+    print(a)
